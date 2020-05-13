d98be4f21735b3b1220923607e9b6681de0ddaa8
SeonJun-Hwang <hsunj2003@gmail.com>
2020-05-13 18:27:53 +0900

    Update: Build Settings
    migrate to typescript
    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,47 @@
 var path = require("path");
+var webpack = require("webpack")
 var HtmlWebpack = require("html-webpack-plugin");
 var TerserWebpack = require("terser-webpack-plugin");
 require("dotenv");
 
 module.exports = {
 	mode: process.env.production ? "production" : "development",
-	entry: {
-		index: ["@babel/polyfill", "./src/index.js"],
-		one: ["@babel/polyfill", "./src/1mb.js"],
-		tag: ["@babel/polyfill", "./src/tags.js"],
-		"key-value": ["@babel/polyfill", "./src/key-value.js"],
-		html: ["@babel/polyfill", "./src/html.js"]
-	},
+	devtool: "eval",
+	entry: ["./src/index.ts"],
 	output: {
-		filename: "[name]-bundle.js",
+		filename: "bundle.js",
 		path: path.join(__dirname, "dist")
+	},
+	resolve: {
+		extensions: [".ts", ".tsx", ".js", ".json"],
+		alias: {
+			"~src": path.resolve(__dirname, "src"),
+			"~style": path.resolve(__dirname, "style"),
+			"~assets": path.resolve(__dirname, "assets"),
+			"~components": path.resolve(__dirname, "src/components"),
+			"~controller": path.resolve(__dirname, "src/controller"),
+			"~api": path.resolve(__dirname, "src/api"),
+			"~model": path.resolve(__dirname, "src/model"),
+			"~utils": path.resolve(__dirname, "src/utils"),
+			"~types": path.resolve(__dirname, "src/types")
+		}
 	},
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
+				test: /\.ts$/,
 				exclude: /node_modules/,
 				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env"]
-					}
+					loader: "ts-loader"
 				}
 			},
 			{
 				test: /\.css$/,
 				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.(jpg|jpeg|gif|svg|png)$/,
+				use: ["file-loader"]
 			}
 		]
 	},
@@ -49,4 +60,16 @@ module.exports = {
 			})
 		]
 	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jquery: 'jquery',
+			"window.jQuery": 'jquery',
+		}),
+		new HtmlWebpack({
+			title: "Index",
+			template: "./public/index.html",
+			filename: "./index.html"
+		})
+	]
 };
